309899f5680060bfbcd15168e43e2a1bad90d6c5
Kazuki Nishikawa <kz.nishikawa@gmail.com>
2021-11-28 16:52:48 +0900

    fix lint

--- rule-based-switch-buffer.el
+++ rule-based-switch-buffer.el
@@ -1,3 +1,32 @@
+;;; rule-based-switch-buffer.el --- Rule based switch buffer framework  -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2021  Kazuki Nishikawa
+
+;; Author: Kazuki Nishikawa <kzkn@hey.com>
+;; Keywords: convenience
+;; Homepage: https://github.com/kzkn/rule-based-switch-buffer
+;; Package-Requires: ((emacs "24.3"))
+;; Package-Version: 0
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; WIP
+
+;;; Code:
+
 (require 'cl-lib)
 
 (defgroup rule-based-switch-buffer nil
@@ -79,3 +108,4 @@
       (find-file selected-file-name))))
 
 (provide 'rule-based-switch-buffer)
+;;; rule-based-switch-buffer.el ends here
