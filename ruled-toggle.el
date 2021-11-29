d75835ea8f01fdf33c18a743c7ad6b85d01b9950
Kazuki Nishikawa <kz.nishikawa@gmail.com>
2021-11-29 21:37:11 +0900

    docstring

--- ruled-toggle.el
+++ ruled-toggle.el
@@ -23,20 +23,16 @@
 
 ;;; Commentary:
 
-;; This library provides a means to define rules for moving between
-;; related buffers, and commands to switch between buffers according
+;; This library provides a macro to define rules for moving between
+;; related buffers, and a command to switch between buffers according
 ;; to the defined rules.
 ;;
-;; For example, you may have rules to switch between .c and .h files,
-;; or between production code and test code.  You define the rules
-;; yourself.  This library does not provide any specific rules.
+;; An example of a switching rule is a rule to switch between .c and
+;; .h file.  Once you have defined such a rule, you will be able to
+;; quickly switch between .c and .h.
 ;;
-;; In some cases, a file may be associated with more than one
-;; file.  For example, foo.h and foo_test.c are candidates for the file
-;; foo.c.  The rule should be to list all possible candidate file
-;; paths.  The library will extract only the file paths that exist from
-;; all the file paths enumerated by the rule.  If multiple candidates
-;; still remain, it will ask you for the file to go to.
+;; You have to define the rules yourself.  This library does not
+;; provide any specific rules.
 
 ;;; Code:
 
@@ -59,6 +55,7 @@
 (defvar ruled-switch-buffer--rules '())
 
 (defun ruled-switch-buffer--flatten (x)
+  "Flatten tree X."
   (cl-labels ((rec (x acc)
                    (cond ((null x) acc)
                          ((atom x) (cons x acc))
@@ -66,6 +63,10 @@
     (rec x nil)))
 
 (defun ruled-switch-buffer--push-rule (name matcher mappers)
+  "Create a new buffer switching rule and register it.
+The rule is named NAME and use MATCHER function as matching
+and use MAPPERS as map from current buffer to the switching
+target file names."
   (let ((rules (cl-remove-if (lambda (rule) (eq name (ruled-switch-buffer--rule-name rule))) ruled-switch-buffer--rules))
         (rule (make-ruled-switch-buffer--rule
                :name name
@@ -74,18 +75,22 @@
     (setq ruled-switch-buffer--rules (cons rule rules))))
 
 (defun ruled-switch-buffer--rule-match-p (rule file-name)
+  "Check the FILE-NAME is matched the RULE."
   (funcall (ruled-switch-buffer--rule-matcher rule) file-name))
 
 (defun ruled-switch-buffer--matched-rules (file-name)
+  "List rules which are matched to FILE-NAME."
   (cl-remove-if-not (lambda (rule)
                       (ruled-switch-buffer--rule-match-p rule file-name))
                     ruled-switch-buffer--rules))
 
 (defun ruled-switch-buffer--mapped-file-names (rule file-name)
+  "List file names which are mapped from FILE-NAME with mappers of RULE."
   (mapcar (lambda (mapper) (funcall mapper file-name))
           (ruled-switch-buffer--rule-mappers rule)))
 
 (defun ruled-switch-buffer--switch-buffer-candidates (file-name)
+  "List file names which are switching targeted by all rules from FILE-NAME."
   (cl-remove-if-not
    #'file-exists-p
    (ruled-switch-buffer--flatten
@@ -93,6 +98,7 @@
             (ruled-switch-buffer--matched-rules file-name)))))
 
 (defun ruled-switch-buffer--select-file-name (candidate-file-names)
+  "Choose a file name from CANDIDATE-FILE-NAMES."
   (if (<= (length candidate-file-names) 1)
       (car candidate-file-names)
     (let ((choices (mapcar #'file-relative-name candidate-file-names)))
@@ -100,6 +106,12 @@
 
 ;;;###autoload
 (cl-defmacro ruled-switch-buffer-define (name &key matcher mappers)
+  "Define a buffer switching rule.
+Argument NAME: A symbol that the name of rule
+Argument MATCHER: A function to check if the rule can be applied
+to the given file path
+Argument MAPPERS: A function or a list of functions to map from
+given file path to the switching target file path"
   (declare (indent defun))
   `(progn
      (ruled-switch-buffer--push-rule ',name #',matcher ',mappers)
@@ -107,6 +119,8 @@
 
 ;;;###autoload
 (defun ruled-switch-buffer ()
+  "Switch buffer from the current buffer according to the defined
+rules."
   (interactive)
   (let* ((file-name (buffer-file-name))
          (candidate-file-names (ruled-switch-buffer--switch-buffer-candidates file-name))
